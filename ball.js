6318ed9cd2816a3993b07510e52a33a8e97f7d70
Gaurav Mall <45404510+GauravMall@users.noreply.github.com>
2022-04-02 19:43:54 +0300

    Update ball.js

--- ball.js
+++ ball.js
@@ -6,11 +6,11 @@ class Ball {
       if (mag != null && angle != null) {
         this.run = true;
         var vel_x = mag * cos(angle)
-      var vel_y = mag * sin(angle)
-      this.velocity = createVector(vel_x, -vel_y)
+        var vel_y = mag * sin(angle)
+        this.velocity = createVector(vel_x, -vel_y)
       
-      this.acceleration = createVector(0, gravity)
-      this.acceleration.setMag(0.1)
+        this.acceleration = createVector(0, gravity)
+        this.acceleration.setMag(0.1)
       } else {
         this.run = false;
       }
@@ -19,15 +19,15 @@ class Ball {
     update() {
       if (this.run) {
         this.velocity.add(this.acceleration)
-      this.pos.add(this.velocity)
+        this.pos.add(this.velocity)
       }
     }
 
     show() {
-        stroke(255)
-        strokeWeight(2)
-        fill(255, 100)
-        ellipse(this.pos.x, this.pos.y, this.size)
+      stroke(255)
+      strokeWeight(2)
+      fill(255, 100)
+      ellipse(this.pos.x, this.pos.y, this.size)
       
       if(this.pos.x > (width - this.size/2) || this.pos.y > (height - this.size / 2)) {
         this.acceleration = createVector();
